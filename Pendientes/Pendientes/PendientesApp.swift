4a46c07ee180378510820417fee68ff1308c3000
Fernando Belen <77164175+FernandoJavierBelen@users.noreply.github.com>
2021-02-05 11:14:09 -0300

    Add files via upload

---
+++ Pendientes/Pendientes/PendientesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PendientesApp.swift
+//  Pendientes
+//
+//  Created by Fernando Belen on 03/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PendientesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
